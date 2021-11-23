80e0e78d9fcba0e2f233ed15b1311cc84ec5b9f5
David Banham <david@banham.id.au>
2021-11-23 05:25:22 +0000

    Add cockroach support to migrate scripts

--- migrations/test_db_reset/main.go
+++ migrations/test_db_reset/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/cockroachdb"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
