9c27a64ddf27e9918e60a8b07d472cbc310b0580
David Banham <david@banham.id.au>
2021-11-25 11:12:42 +0000

    Add full text index to Thing

--- migrations/1524564146_thing.up.sql
+++ migrations/1524564146_thing.up.sql
@@ -2,5 +2,14 @@ CREATE TABLE things (
   id UUID PRIMARY KEY,
   revision TEXT NOT NULL UNIQUE,
   name TEXT NOT NULL,
+  description TEXT NOT NULL,
   organisation_id UUID REFERENCES organisations (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+ALTER TABLE things ADD COLUMN ts tsvector
+  GENERATED ALWAYS AS
+    (  to_tsvector('english', coalesce(name, ''))
+    || to_tsvector('english', coalesce(description, ''))
+  ) STORED;
+
+CREATE INDEX ts_idx ON things USING GIN (ts);
