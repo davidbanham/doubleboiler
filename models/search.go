fd2a7977ba03897764b3ec6c699eb0d892373d84
David Banham <david@banham.id.au>
2021-11-26 03:32:38 +0000

    Improve search results

--- models/search.go
+++ models/search.go
@@ -11,6 +11,7 @@ type SearchResult struct {
 	EntityType string
 	ID         string
 	Label      string
+	Rank       float64
 }
 
 type SearchResults struct {
@@ -31,8 +32,8 @@ func (results *SearchResults) FindAll(ctx context.Context, q Query) error {
 		return fmt.Errorf("Unknown query")
 	case ByPhrase:
 		rows, err = db.QueryContext(ctx, `SELECT
-		id, entity_type, label, uri_path
-		FROM search_items WHERE organisation_id = $1 AND ts @@ phraseto_tsquery('english', $2)`, v.OrgID, v.Phrase)
+		id, entity_type, label, uri_path, ts_rank_cd(ts, query) AS rank
+		FROM search_items, plainto_tsquery('english', $2) query WHERE organisation_id = $1 AND query @@ ts ORDER BY rank DESC`, v.OrgID, v.Phrase)
 	}
 	if err != nil {
 		return err
@@ -42,7 +43,7 @@ func (results *SearchResults) FindAll(ctx context.Context, q Query) error {
 	for rows.Next() {
 		result := SearchResult{}
 		err = rows.Scan(
-			&result.ID, &result.EntityType, &result.Label, &result.Path,
+			&result.ID, &result.EntityType, &result.Label, &result.Path, &result.Rank,
 		)
 		if err != nil {
 			return err
