a9d5b5a0449e9e042b3e5f7bde11d0835cdd6f1d
David Banham <david@banham.id.au>
2021-11-27 12:58:58 +0000

    Allow superadmins to do anything

--- routes/authorisation.go
+++ routes/authorisation.go
@@ -78,6 +78,10 @@ func authFreeMiddleware(h http.Handler) http.Handler {
 }
 
 func can(ctx context.Context, target models.Organisation, role string) bool {
+	if isAppAdmin(ctx) {
+		return true
+	}
+
 	unconv := ctx.Value("organisation_users")
 	if unconv == nil {
 		return false
