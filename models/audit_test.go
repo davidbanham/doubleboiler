fc40ccb66548687aa92b222489106644859780d9
David Banham <david@banham.id.au>
2023-12-01 12:33:28 +1100

    Add audit query test

--- models/audit_test.go
+++ models/audit_test.go
@@ -21,3 +21,21 @@ func TestAuditsByOrg(t *testing.T) {
 
 	closeTx(t, ctx)
 }
+
+func TestAuditsByEntity(t *testing.T) {
+	t.Parallel()
+	ctx := getCtx(t)
+
+	org := organisationFixture()
+	org.Save(ctx)
+
+	fix := userFixture()
+	assert.Nil(t, fix.Save(ctx))
+
+	audits := Audits{}
+	criteria := Criteria{}
+	AddCustomQuery(ByEntityID{EntityID: fix.ID}, &criteria)
+	assert.Nil(t, audits.FindAll(ctx, criteria))
+
+	closeTx(t, ctx)
+}
